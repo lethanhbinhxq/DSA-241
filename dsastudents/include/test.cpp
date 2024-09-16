c19b3746cd50cfa6512d742975a51b3249a8824d
lethanhbinhxq <binh.lethanh@hcmut.edu.vn>
2024-09-17 06:56:11 +0700

    Implement List + TensorDataset

---
+++ dsastudents/include/test.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include "list/XArrayListDemo.h"
+#include "list/DLinkedListDemo.h"
+
+using namespace std;
+
+
+int main(int argc, char** argv) {
+    xlistDemo1();
+    xlistDemo2();
+    xlistDemo3();
+    xlistDemo4();
+
+    dlistDemo1();
+    dlistDemo2();
+    dlistDemo3();
+    dlistDemo4();
+    dlistDemo5();
+    dlistDemo6();
+    return 0;
+}
+
